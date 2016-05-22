307002a6a4e43a44cbae4aaca963108f0c19cb05
Harrison Melton <hmmelton95@gmail.com>
2016-05-22 12:40:26 -0700

    removed unnecessary animations, added animations to login screen

--- app/src/main/java/com/hmmelton/pasttime/LoginActivity.java
+++ app/src/main/java/com/hmmelton/pasttime/LoginActivity.java
@@ -4,12 +4,17 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.view.animation.DecelerateInterpolator;
 
 public class LoginActivity extends AppCompatActivity {
 
     private final int ANIM_DURATION = 600; // duration - in ms - of animation
+    private View mContentView;
+    private View mFbLogin;
+    private View mTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,32 +22,96 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // content view - contains sub-views for activity
+        mContentView = findViewById(R.id.content_login);
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-
-        // content view - contains sub-views for activity
-        View contentView = findViewById(R.id.content_login);
+    protected void onResume() {
+        super.onResume();
 
         // find bars on bottom of screen
-        final View topBar = contentView.findViewById(R.id.login_top_bar);
-        final View middleBar = contentView.findViewById(R.id.login_middle_bar);
-        View bottomBar = contentView.findViewById(R.id.login_bottom_bar);
+        View topBar = mContentView.findViewById(R.id.login_top_bar);
+        View middleBar = mContentView.findViewById(R.id.login_middle_bar);
+        View bottomBar = mContentView.findViewById(R.id.login_bottom_bar);
 
-        Animation bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_bar_slide_in);
+        mFbLogin = mContentView.findViewById(R.id.facebook_login_button);
+
+        View topShape = mContentView.findViewById(R.id.login_top_shape);
+        mTitle = mContentView.findViewById(R.id.login_title);
+
+
+        // animation that fades in views
+        final Animation fadeIn = new AlphaAnimation(0, 1);
+        fadeIn.setInterpolator(new DecelerateInterpolator());
+        fadeIn.setDuration(ANIM_DURATION * 2);
+
+        // animation that slides top portion down from top
+        Animation slideFromTop = AnimationUtils.loadAnimation(this, R.anim.slide_in_from_top);
+        slideFromTop.setInterpolator(new DecelerateInterpolator());
+        slideFromTop.setDuration(ANIM_DURATION * 2);
+        slideFromTop.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                mTitle.setAlpha(1.0f);
+                mTitle.startAnimation(fadeIn);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        //**********************************************************************
+        // These animations are for the sliding bars on the bottom of the screen
+        //**********************************************************************
+        Animation bottomAnim = AnimationUtils.loadAnimation(this, R.anim.white_bar_slide_in);
         bottomAnim.setDuration(ANIM_DURATION);
 
-        Animation middleAnim = AnimationUtils.loadAnimation(this, R.anim.middle_bar_slide_in);
+        Animation middleAnim = AnimationUtils.loadAnimation(this, R.anim.white_bar_slide_in);
         middleAnim.setDuration(ANIM_DURATION);
+        middleAnim.setStartOffset(ANIM_DURATION / 2);
 
-        Animation topAnim = AnimationUtils.loadAnimation(this, R.anim.top_bar_slide_in);
+        Animation topAnim = AnimationUtils.loadAnimation(this, R.anim.white_bar_slide_in);
         topAnim.setDuration(ANIM_DURATION);
+        topAnim.setStartOffset(ANIM_DURATION);
+        topAnim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                mFbLogin.setAlpha(1.0f);
+                mFbLogin.startAnimation(fadeIn);
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
 
         bottomBar.startAnimation(bottomAnim);
         middleBar.startAnimation(middleAnim);
         topBar.startAnimation(topAnim);
+        topShape.startAnimation(slideFromTop);
 
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mTitle.setAlpha(0.0f);
+        mFbLogin.setAlpha(0.0f);
     }
 }
